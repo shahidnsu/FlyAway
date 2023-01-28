0356b4e126907c60c11a88c42e2262d83eccfb21
2apreety18 <2apreety@gmail.com>
2023-01-28 23:44:59 +0600

    installed material, lottie and done landing page

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { LottieModule } from 'ngx-lottie';
+import player from 'lottie-web';
+import { SplashScreenComponent } from './components/splash-screen/splash-screen.component';
+
+export function playerFactory(): any {  
+  return import('lottie-web');
+}
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SplashScreenComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    LottieModule.forRoot({ player: playerFactory }),  
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ client/src/app/components/splash-screen/splash-screen.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { AnimationItem } from 'lottie-web';
+import { AnimationOptions } from 'ngx-lottie';
+
+@Component({
+  selector: 'app-splash-screen',
+  templateUrl: './splash-screen.component.html',
+  styleUrls: ['./splash-screen.component.css']
+})
+export class SplashScreenComponent {
+  options: AnimationOptions = {    
+    path: './assets/19417-no-place-like-home-flight.json'  
+  };  
+
+  constructor() { }  
+
+  onAnimate(animationItem: AnimationItem): void {    
+    console.log(animationItem);  
+  }
+}
