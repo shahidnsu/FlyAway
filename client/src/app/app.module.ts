60ad78a02d533a0bfe60b4b315119b17ad27f00d
2apreety18 <2apreety@gmail.com>
2023-01-29 11:10:53 +0600

    Frontend page design
    Login page design done

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { LottieModule } from 'ngx-lottie';
 import { SplashScreenComponent } from './components/splash-screen/splash-screen.component';
 import { LoginComponent } from './components/login/login.component';
-import {MatCardModule} from '@angular/material/card';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule  } from '@angular/material/form-field';
+import { MatInputModule} from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+
 
 export function playerFactory(): any {  
   return import('lottie-web');
@@ -27,9 +31,12 @@ export function playerFactory(): any {
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    ReactiveFormsModule,
     LottieModule.forRoot({ player: playerFactory }),  
     MatCardModule,
-    MatFormFieldModule
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
