a3ce531a8e08928c1afa9fa2670eafb465dfe78e
Kh Shuhat Uddin <shuhat1971@gmail.com>
2023-01-31 17:17:15 +0600

    Feat: AutoComplete added for To

--- client/src/app/components/select-date-and-places/select-date-and-places.component.ts
+++ client/src/app/components/select-date-and-places/select-date-and-places.component.ts
@@ -28,12 +28,25 @@ export class SelectDateAndPlacesComponent implements OnInit {
   },
   {
     "name": "London Airport",
-    "city": "London",
+    "city": "london",
+    "iata": "YXU"
+  }
+]
+
+  toLocationArray = [{
+    "name":"Shahjalal Int Airport",
+    "city":"dhaka",
+    "iata":"DAC"
+  },
+  {
+    "name": "London Airport",
+    "city": "london",
     "iata": "YXU"
   }
 ]
 
   filteredOption!: Observable<any>;
+  filteredOptionTo!: Observable<any>;
 
   ngOnInit(){
     this.travelForm = new FormGroup({
@@ -50,20 +63,27 @@ export class SelectDateAndPlacesComponent implements OnInit {
       console.log('child',value)
       Object.assign(this.newLeg, value)
       console.log('parent from child', this.newLeg);
-
-
     })
-    
+
     this.filteredOption = this.travelForm.valueChanges.pipe(
       map(value => this._filter(value))
     );
 
+    this.filteredOptionTo = this.travelForm.valueChanges.pipe(
+      map(value => this._tofilter(value))
+    );
+
     this.filteredOption.subscribe(value => console.log(value));
-    
+    this.filteredOptionTo.subscribe(value => console.log(value));
   }
 
   private _filter(value: any): any {
     return this.locationArray.filter(location => location.city.includes(value.from));
+  }
+
+
+  private _tofilter(value: any): any {
+    return this.toLocationArray.filter(location => location.city.includes(value.to));
   }
 
   
