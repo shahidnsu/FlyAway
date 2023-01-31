47d6ed19392f6b0d514312534fdbf226f243fd39
2apreety18 <2apreety@gmail.com>
2023-01-31 15:19:52 +0600

    added mock data in confirm flights

--- client/src/app/pages/confirm-flights-page/confirm-flights-page.component.ts
+++ client/src/app/pages/confirm-flights-page/confirm-flights-page.component.ts
@@ -6,16 +6,52 @@ import { Component } from '@angular/core';
   styleUrls: ['./confirm-flights-page.component.css']
 })
 export class ConfirmFlightsPageComponent {
-  // confirmFlights = [
-  //   from {
-  //     time: "5.00PM",
-  //     iataCode: "DEL",
-  //     airportName:"Indira Gandhi International Airport",
-  //   }
-  //   to {
-  //     time: "7.30PM",
-  //     iataCode: "CCU",
-  //     airportName:"Subhash Chandra Bose International Airport",
-  //   }
-  // ];
+  confirmFlights = [
+    {
+      from: {
+        time: "5.00PM",
+        iataCode: "DEL",
+        airportName:"Indira Gandhi International Airport",
+      },
+      to: {
+        time: "7.30PM",
+        iataCode: "CCU",
+        airportName:"Subhash Chandra Bose International Airport",
+      },
+      travelDate: "01/31/2023",
+      duration: "09:30",
+      price: "$230"
+    },
+
+    {
+      from: {
+        time: "4.00PM",
+        iataCode: "DEL",
+        airportName:"Indira Gandhi International Airport",
+      },
+      to: {
+        time: "6.30PM",
+        iataCode: "CCU",
+        airportName:"Subhash Chandra Bose International Airport",
+      },
+      travelDate: "MM/DD/YYYY",
+      duration: "09:30",
+      price: "$230"
+    },
+    {
+      from: {
+        time: "3.00PM",
+        iataCode: "DEL",
+        airportName:"Indira Gandhi International Airport",
+      },
+      to: {
+        time: "5.30PM",
+        iataCode: "CCU",
+        airportName:"Subhash Chandra Bose International Airport",
+      },
+      travelDate: "MM/DD/YYYY",
+      duration: "09:30",
+      price: "$230"
+    }
+  ];
 }
