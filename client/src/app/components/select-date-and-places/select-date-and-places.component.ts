7603e55ec1f12ae88907f873cb6fabf2e46d0047
Kh Shuhat Uddin <shuhat1971@gmail.com>
2023-01-31 16:18:05 +0600

    Autocomplete working for From

--- client/src/app/components/select-date-and-places/select-date-and-places.component.ts
+++ client/src/app/components/select-date-and-places/select-date-and-places.component.ts
@@ -1,5 +1,7 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormControl,FormGroup } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
 
 @Component({
   selector: 'app-select-date-and-places',
@@ -16,7 +18,22 @@ export class SelectDateAndPlacesComponent implements OnInit {
   travelFormArray: any
   travelForm!: FormGroup;
 
+  
   @Input() newLeg!: Object;
+  
+  locationArray = [{
+    "name":"Kampong Chhnang Airport",
+    "city":"sanghai",
+    "iata":"KZC"
+  },
+  {
+    "name": "London Airport",
+    "city": "London",
+    "iata": "YXU"
+  }
+]
+
+  filteredOption!: Observable<any>;
 
   ngOnInit(){
     this.travelForm = new FormGroup({
@@ -24,14 +41,29 @@ export class SelectDateAndPlacesComponent implements OnInit {
       to: new FormControl(''),
       date: new FormControl('')
     })
+
     if(this.travelFormArray.length>1){
       this.travelForm.controls['from'].setValue(this.travelFormArray[this.travelFormArray.length-1].to)
     }
+
     this.travelForm.valueChanges.subscribe((value)=>{
       console.log('child',value)
       Object.assign(this.newLeg, value)
-      console.log('parent from child', this.newLeg)
+      console.log('parent from child', this.newLeg);
+
+
     })
+    
+    this.filteredOption = this.travelForm.valueChanges.pipe(
+      map(value => this._filter(value))
+    );
+
+    this.filteredOption.subscribe(value => console.log(value));
+    
+  }
+
+  private _filter(value: any): any {
+    return this.locationArray.filter(location => location.city.includes(value.from));
   }
 
   
