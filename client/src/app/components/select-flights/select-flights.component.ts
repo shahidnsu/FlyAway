e4a19399d4c389f41c359ab7e8e17c07d3043995
2apreety18 <2apreety@gmail.com>
2023-02-01 19:34:14 +0600

    selected flight selection done

--- client/src/app/components/select-flights/select-flights.component.ts
+++ client/src/app/components/select-flights/select-flights.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Flight } from 'src/app/interfaces/flight';
 
 @Component({
   selector: 'app-select-flights',
@@ -6,7 +7,15 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./select-flights.component.css']
 })
 export class SelectFlightsComponent {
+  
+  @Input() selected!: boolean;
+  @Output() selectFlightEvent = new EventEmitter();
+  
+  // @Input() selectedFlights;
+  @Input() flights!: Flight;
 
-  @Input() selectedFlights: any;
+  handleClick () {
+    this.selectFlightEvent.emit(this.flights);
+  }
 
 }
--- client/src/app/pages/select-flights-page/select-flights-page.component.ts
+++ client/src/app/pages/select-flights-page/select-flights-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Flight } from 'src/app/interfaces/flight';
 
 @Component({
   selector: 'app-select-flights-page',
@@ -7,55 +8,186 @@ import { Component } from '@angular/core';
 })
 export class SelectFlightsPageComponent {
 
-  selectedFlights = [
-    {
-      from: {
-        time: "5.00PM",
-        iataCode: "DEL",
-        airportName:"Indira Gandhi International Airport",
-      },
-      to: {
-        time: "7.30PM",
-        iataCode: "CCU",
-        airportName:"Subhash Chandra Bose International Airport",
-      },
-      travelDate: "01/31/2023",
-      carrierCode: "6E",
-      price: "$230"
-    },
+  flights!: Flight[];
 
-    {
-      from: {
-        time: "4.00PM",
-        iataCode: "DEL",
-        airportName:"Indira Gandhi International Airport",
-      },
-      to: {
-        time: "6.30PM",
-        iataCode: "CCU",
-        airportName:"Subhash Chandra Bose International Airport",
-      },
-      travelDate: "MM/DD/YYYY",
-      carrierCode: "BG",
-      price: "$230"
-    },
-    {
-      from: {
-        time: "3.00PM",
-        iataCode: "DEL",
-        airportName:"Indira Gandhi International Airport",
-      },
-      to: {
-        time: "5.30PM",
-        iataCode: "CCU",
-        airportName:"Subhash Chandra Bose International Airport",
-      },
-      travelDate: "MM/DD/YYYY",
-      carrierCode: "BS",
-      price: "$230"
-    }
-  ];
+  selectedFlight: Flight[] = [];
 
+
+  searchResults = [[
+      {
+        segments: [
+            {
+                departure: {
+                    iataCode: "DAC",
+                    at: "2023-03-04T13:00:00"
+                },
+                arrival: {
+                    iataCode: "CMB",
+                    at: "2023-03-04T15:55:00"
+                }
+            },
+            {
+                departure: {
+                    iataCode: "CMB",
+                    at: "2023-03-04T18:15:00"
+                },
+                arrival: {
+                    iataCode: "RUH",
+                    at: "2023-03-04T21:30:00"
+                }
+            }
+        ],
+        price: 436.67
+      },
+      {
+        segments: [
+            {
+                departure: {
+                    iataCode: "DAC",
+                    at: "2023-03-04T13:00:00"
+                },
+                arrival: {
+                    iataCode: "KWI",
+                    at: "2023-03-04T15:55:00"
+                }
+            },
+            {
+                departure: {
+                    iataCode: "KEI",
+                    at: "2023-03-04T18:15:00"
+                },
+                arrival: {
+                    iataCode: "RUH",
+                    at: "2023-03-04T21:30:00"
+                }
+            }
+        ],
+        price: 136.67
+      }    
+  ],
+    [
+        {
+            segments: [
+                {
+                    departure: {
+                        iataCode: "RUH",
+                        at: "2023-03-04T13:00:00"
+                    },
+                    arrival: {
+                        iataCode: "IND",
+                        at: "2023-03-04T15:55:00"
+                    }
+                },
+                {
+                    departure: {
+                        iataCode: "IND",
+                        at: "2023-03-04T18:15:00"
+                    },
+                    arrival: {
+                        iataCode: "USA",
+                        at: "2023-03-04T21:30:00"
+                    }
+                }
+            ],
+            price: 436.67
+          },
+          {
+            segments: [
+                {
+                    departure: {
+                        iataCode: "RUH",
+                        at: "2023-03-04T13:00:00"
+                    },
+                    arrival: {
+                        iataCode: "KEI",
+                        at: "2023-03-04T15:55:00"
+                    }
+                },
+                {
+                    departure: {
+                        iataCode: "KEI",
+                        at: "2023-03-04T18:15:00"
+                    },
+                    arrival: {
+                        iataCode: "USA",
+                        at: "2023-03-04T21:30:00"
+                    }
+                }
+            ],
+            price: 136.67
+          } 
+    ]];
+
+
+
+
+
+
+
+
+
+
+
+  // selectedFlights = [
+  //   {
+  //     from: {
+  //       time: "5.00PM",
+  //       iataCode: "DEL",
+  //       airportName:"Indira Gandhi International Airport",
+  //     },
+  //     to: {
+  //       time: "7.30PM",
+  //       iataCode: "CCU",
+  //       airportName:"Subhash Chandra Bose International Airport",
+  //     },
+  //     travelDate: "01/31/2023",
+  //     carrierCode: "6E",
+  //     price: "$230"
+  //   },
+
+  //   {
+  //     from: {
+  //       time: "4.00PM",
+  //       iataCode: "DEL",
+  //       airportName:"Indira Gandhi International Airport",
+  //     },
+  //     to: {
+  //       time: "6.30PM",
+  //       iataCode: "CCU",
+  //       airportName:"Subhash Chandra Bose International Airport",
+  //     },
+  //     travelDate: "MM/DD/YYYY",
+  //     carrierCode: "BG",
+  //     price: "$230"
+  //   },
+  //   {
+  //     from: {
+  //       time: "3.00PM",
+  //       iataCode: "DEL",
+  //       airportName:"Indira Gandhi International Airport",
+  //     },
+  //     to: {
+  //       time: "5.30PM",
+  //       iataCode: "CCU",
+  //       airportName:"Subhash Chandra Bose International Airport",
+  //     },
+  //     travelDate: "MM/DD/YYYY",
+  //     carrierCode: "BS",
+  //     price: "$230"
+  //   }
+  // ];
+
+  handleSelect (flight: Flight) {
+    
+    const newFlightList = this.selectedFlight.filter(fl => {
+        if(flight.segments[0].departure.iataCode === fl.segments[0].departure.iataCode
+            && flight.segments[flight.segments.length - 1].arrival.iataCode === fl.segments[flight.segments.length - 1].arrival.iataCode) {
+                return false;
+            } else return true
+    })
+
+    this.selectedFlight = [...newFlightList, flight];
+  }
 
 
 }
