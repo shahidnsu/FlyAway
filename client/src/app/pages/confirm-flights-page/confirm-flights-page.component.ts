97b5ed2c638a35c9c65a503933882ffcc01b7354
2apreety18 <2apreety@gmail.com>
2023-02-02 10:56:05 +0600

    removed merge conflict on selected and confirm pages

--- client/src/app/pages/confirm-flights-page/confirm-flights-page.component.ts
+++ client/src/app/pages/confirm-flights-page/confirm-flights-page.component.ts
@@ -5,7 +5,7 @@ import { Flight } from 'src/app/interfaces/flight';
 @Component({
   selector: 'app-confirm-flights-page',
   templateUrl: './confirm-flights-page.component.html',
-  styleUrls: ['./confirm-flights-page.component.css'],
+  styleUrls: ['./confirm-flights-page.component.css']
 })
 export class ConfirmFlightsPageComponent {
   confirmedFlight: Flight[] = [];
@@ -65,11 +65,66 @@ export class ConfirmFlightsPageComponent {
           } 
     ];
 
+  // confirmFlights = [
+  //   {
+  //     from: {
+  //       time: "5.00PM",
+  //       iataCode: "DEL",
+  //       airportName:"Indira Gandhi International Airport",
+  //     },
+  //     to: {
+  //       time: "7.30PM",
+  //       iataCode: "CCU",
+  //       airportName:"Subhash Chandra Bose International Airport",
+  //     },
+  //     travelDate: "01/31/2023",
+  //     duration: "09:30",
+  //     price: "$230"
+  //   },
+
+  //   {
+  //     from: {
+  //       time: "4.00PM",
+  //       iataCode: "DEL",
+  //       airportName:"Indira Gandhi International Airport",
+  //     },
+  //     to: {
+  //       time: "6.30PM",
+  //       iataCode: "CCU",
+  //       airportName:"Subhash Chandra Bose International Airport",
+  //     },
+  //     travelDate: "MM/DD/YYYY",
+  //     duration: "09:30",
+  //     price: "$230"
+  //   },
+  //   {
+  //     from: {
+  //       time: "3.00PM",
+  //       iataCode: "DEL",
+  //       airportName:"Indira Gandhi International Airport",
+  //     },
+  //     to: {
+  //       time: "5.30PM",
+  //       iataCode: "CCU",
+  //       airportName:"Subhash Chandra Bose International Airport",
+  //     },
+  //     travelDate: "MM/DD/YYYY",
+  //     duration: "09:30",
+  //     price: "$230"
+  //   }
+  // ];
 
   isCompleted=true;
   isLinear = true;
 
   constructor(private route: Router) {}
+
+
+  // handleConfirm (flight: Flight) {
+  //   this.confirmedFlight.push(flight);
+  //   console.log(this.confirmedFlight);
+  // }
+
 
   confirm() {
     this.route.navigate(['payment']);
