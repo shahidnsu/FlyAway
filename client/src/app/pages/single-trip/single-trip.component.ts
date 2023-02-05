62be347ce2d7af98f49f12dd6b34898c6b4e68ca
2apreety18 <2apreety@gmail.com>
2023-02-05 19:10:19 +0600

    updated trip history page

--- client/src/app/pages/single-trip/single-trip.component.ts
+++ client/src/app/pages/single-trip/single-trip.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TripService } from 'src/app/service/trip.service';
 
 @Component({
   selector: 'app-single-trip',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./single-trip.component.css']
 })
 export class SingleTripComponent {
+  trip: any;
 
+  constructor(private tripService: TripService) {}
+ 
+  ngOnInit() {
+    this.trip = this.tripService.getTrip()
+  }
 }
