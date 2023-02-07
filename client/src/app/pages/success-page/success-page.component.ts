b8d8aba7f1fd1adcb20e120dd8ee9c9d0dd0d59e
Shahidnsu <shahid.khan@northsouth.edu>
2023-02-07 18:51:22 +0600

    everything is working

--- client/src/app/pages/success-page/success-page.component.ts
+++ client/src/app/pages/success-page/success-page.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ApiClientService } from 'src/app/service/api-client.service';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-success-page',
@@ -6,5 +9,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./success-page.component.css']
 })
 export class SuccessPageComponent {
+  constructor(private route: Router, private flightService: ApiClientService,
+    private http:HttpClient) {}
 
+
+  selectedFlights = JSON.parse(localStorage.getItem("trip")!)
+  ngOnInit() {
+
+
+    this.flightService.createTripList(this.selectedFlights).subscribe(res=>console.log(res));
+
+}
 }
