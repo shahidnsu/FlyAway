80d3931e671e58dadf15c8d61d05abf2b890a4b9
Shahidnsu <shahid.khan@northsouth.edu>
2023-02-07 16:34:36 +0600

    node mailer is working fine

--- server/controllers/trip.js
+++ server/controllers/trip.js
@@ -4,18 +4,18 @@ const TripList = require('../models/tripList');
 
 //creating email options for the 
 
-const creatingMailOptions = function() {
-   let userEmail = localStorage.getItem("user")
+const creatingMailOptions = function(email,id) {
+   
     
     let mailOptions = {
       from: "hello.hr.portal@gmail.com",
-      to: userEmail,
+      to:email ,
       subject: "Your trip is  booked",
-      text: `
+      html: `
       <div style="background: transparent linear-gradient(90deg, #2BAD90 0%, #00489A 100%) 0% 0% no-repeat padding-box;
       background: transparent linear-gradient(90deg, #2BAD90 0%, #00489A 100%) 0% 0% no-repeat padding-box;
       width: 497px; height: 133px; color: white; padding: 1px 0 10px 0;">
-  <h1 style="text-align: center; font-weight: 100; font-size: 20px ; line-height: 57px;  font-family: Verdana; margin: 0px;">Thank you  for Booking trip with flyAway</h1>
+  <h1 style="text-align: center; font-weight: 100; font-size: 20px ; line-height: 57px;  font-family: Verdana; margin: 0px;">Thank you  for Booking trip with flyAway    ${id}</h1>
   <h3 style="text-align: center; font-weight: 100; font-size: 50px ; line-height: 57px;  font-family: Verdana;  margin: 0px;">Enjoy your trip and give us your feedback how you can improve our self</h3>
 </div>
 <br>
@@ -27,11 +27,16 @@ const creatingMailOptions = function() {
 }
 
 const createTrip = async (req, res) => {
+    console.log()
     try {
-        
+        console.log('Inside try')
+        const {email} = req.user
+        console.log(email)
         const result = await TripList.create(req.body).then(result => {
-            return creatingMailOptions()
+            console.log("inside then block")
+            return creatingMailOptions(email,result._id)
         }).then(mailOptions => {
+            console.log("inside next then ")
             transport(mailOptions)
         })
         
