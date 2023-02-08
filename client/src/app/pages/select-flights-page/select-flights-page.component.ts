6651e21b8d364446caff86b81a99dd5e061fed6d
Kh Shuhat Uddin <shuhat1971@gmail.com>
2023-02-08 12:26:43 +0600

    responsiveness done

--- client/src/app/pages/select-flights-page/select-flights-page.component.ts
+++ client/src/app/pages/select-flights-page/select-flights-page.component.ts
@@ -33,16 +33,6 @@ export class SelectFlightsPageComponent {
   }
 
   ngOnInit() {
-    //   setTimeout(() => {
-    //     this.searchResults = this._FlightService.getSearchedFlights();
-    //     this.isLoading = false;
-    //     console.log("Selected Flights: ", this.searchResults);
-    //   }, 5000)
-    // }
-    // this._FlightService.getSearchedFlights().subscribe((flights) => {
-    //   this.searchResults = flights;
-    //   if(this.searchResults.length) this.isLoading = false;
-    // })
 
     this.searchResults = this._FlightService.getSearchedFlights();
   }
