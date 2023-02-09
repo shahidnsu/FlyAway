72065582d3ab6da2dc043c838c78d81bfac28e45
akramRickon <akramhsin29@gmail.com>
2023-02-09 11:57:35 +0600

    merge conflit solved

--- client/src/test.ts
+++ client/src/test.ts
@@ -1,15 +1,10 @@
 import 'jest-preset-angular/setup-jest';
 
-<<<<<<< HEAD
 Object.defineProperty(window, 'CSS', {value: null});
-=======
-Object.defineProperty(window, 'CSS', { value: null });
->>>>>>> b4ed4300b0003f593616bdf5bbefb82e4fdb6b29
 Object.defineProperty(window, 'getComputedStyle', {
   value: () => {
     return {
       display: 'none',
-<<<<<<< HEAD
       appearance: ['-webkit-appearance']
     };
   }
@@ -17,28 +12,12 @@ Object.defineProperty(window, 'getComputedStyle', {
 
 Object.defineProperty(document, 'doctype', {
   value: '<!DOCTYPE html>'
-=======
-      appearance: ['-webkit-appearance'],
-    };
-  },
-});
-
-Object.defineProperty(document, 'doctype', {
-  value: '<!DOCTYPE html>',
->>>>>>> b4ed4300b0003f593616bdf5bbefb82e4fdb6b29
 });
 Object.defineProperty(document.body.style, 'transform', {
   value: () => {
     return {
       enumerable: true,
-<<<<<<< HEAD
       configurable: true
     };
   }
 });
-=======
-      configurable: true,
-    };
-  },
-});
->>>>>>> b4ed4300b0003f593616bdf5bbefb82e4fdb6b29
