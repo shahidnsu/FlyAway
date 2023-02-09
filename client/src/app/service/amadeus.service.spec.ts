5373c64eed2a29701956b2976d130463d94ca1c7
Shahidnsu <shahid.khan@northsouth.edu>
2023-02-09 09:13:16 +0600

    statred testing application

--- client/src/app/service/amadeus.service.spec.ts
+++ client/src/app/service/amadeus.service.spec.ts
@@ -1,16 +1,47 @@
-import { TestBed } from '@angular/core/testing';
-
+// @ts-nocheck
+import { of } from 'rxjs';
+import '@angular/compiler';
 import { AmadeusService } from './amadeus.service';
-
+import { HttpClient } from '@angular/common/http';
+import { Injectable, ReflectiveInjector } from '@angular/core';
+import { Airport } from '../interfaces/airport';
+import { TestBed } from '@angular/core/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+import { MockService } from 'ng-mocks';
+import { Observable } from 'rxjs';
+const airportsMocks = [
+  {
+    name: 'Londolovit Airport',
+    city: 'Londolovit',
+    iata: 'LNV',
+  },
+];
+let apiClient: HttpClient;
 describe('AmadeusService', () => {
+  const http = { get: jest.fn(() => of(airportsMocks)) };
   let service: AmadeusService;
-
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(AmadeusService);
+  const provide = (mock: any): any => mock;
+  service = new AmadeusService(provide(http) as any);
+  test('should fetch a list of airports', () => {
+    let result = service.airportSearch('dhaka').subscribe((airports) => {
+      // expect(http.get).toHaveBeenCalled();
+      expect(http.get).toHaveBeenCalledWith(
+        'http://localhost:3000/aiport/dhaka'
+      );
+    });
   });
-
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('should fetch a list of airports with given iata code', () => {
+    service.airportRoute('dhaka').subscribe((airports) => {
+      console.log(airports);
+      expect(http.get).toHaveBeenCalled();
+    });
+  });
+  it('should fetch a list of options', () => {
+    service.searchFlight('dhaka').subscribe((airports) => {
+      console.log(airports);
+    });
   });
 });
