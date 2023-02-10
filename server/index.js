e4449dba7ce481dd883e43e0cfba9263bca6ba36
Shahidnsu <shahid.khan@northsouth.edu>
2023-02-10 14:03:23 +0600

    env updated

--- server/index.js
+++ server/index.js
@@ -1,41 +1,44 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const bodyParser = require('body-parser')
+const bodyParser = require("body-parser");
 const router = require("./router");
 const dotenv = require("dotenv");
-const { mongoose, uri } = require("./db");
-const PORT = process.env.SERVER_PORT || 3000;
-app.use(express.static('public'))
+dotenv.config();
+const { mongoose } = require("./db");
+const uri = process.env.DB;
+const PORT = process.env.SERVER_PORT;
 
-const corsConfig = {
-    origin: "http://localhost:4200",
-    credentials: true,
-    exposedHeaders: "Authorization",
+const corsOptions = {
+  origin: [
+    "http://localhost:4200",
+    "https://fly-away.vercel.app",
+    "https://flyaway.vercel.app",
+    "https://flyaway-drab.vercel.app/",
+  ],
+  credentials: true,
+  exposedHeaders: "Authorization",
 };
 
-dotenv.config();
-app.use(cors(corsConfig));
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(router);
-app.use(bodyParser.json())
-
-
+app.use(bodyParser.json());
 
 // handling if routes not found
 app.get("*", (req, res) => {
-    res.status(404);
-    res.send("Server not found");
+  res.status(404);
+  res.send("Server not found");
 });
 
 (async function bootstrap() {
-    try {
-        await mongoose.connect(uri);
-        console.log("Database Connected");
-        app.listen(PORT, () => {
-            console.log(`Server running on ${PORT}`);
-        });
-    } catch (error) {
-        console.log(error);
-    }
+  try {
+    await mongoose.connect(uri);
+    console.log("Database Connected");
+    app.listen(PORT, () => {
+      console.log(`Server running on ${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
 })();
